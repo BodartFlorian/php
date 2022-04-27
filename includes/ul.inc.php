90b751f289769549258f5c9858e2351ed24fe919
FlorianBodart <bodart.florian@outlook.fr>
2022-04-27 17:01:10 +0200

    version 0.1.0 ajout début formulaire

--- includes/ul.inc.php
+++ includes/ul.inc.php
@@ -1,4 +1,4 @@
-<nav>
+<!-- <nav>
     <button type="button" class="btn btn-outline-secondary">Home
         </button>
         <ul>
@@ -8,20 +8,40 @@
             <li>Fonction</li>
             <li>Supprimer</li>
         </ul>
-</nav>
-    
+</nav> -->
+
+<div class="container">
+    <div class="d-flex p-2 bd-highlight w-25 p-3" >
+
+        <nav>
+            <button type="button" class="btn btn-outline-secondary w-100 p-2" >Home </button>
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <button type="button" class="list-group-item list-group-item-action">A second item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                </ul>
+        </nav>
+
+    </div>
+</div>
 
 
-   
-<button class="btn btn-outline-secondary"
-         type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Home
-       
-        <ul>
-            <li>Debogage</li>
-            <li>Concaténation</li>
-            <li>Boucle</li>
-            <li>Fonction</li>
-            <li>Supprimer</li>
-        </ul>
- </button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,6 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-include 'includes/head.inc.html';
-include 'includes/header.inc.html';
-include 'includes/ul.inc.php';
+    <?php  include 'includes/head.inc.html';  ?>
+<body>
+     <?php include 'includes/header.inc.html';  ?>
+     
 
+     <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <nav>
+                    <div class="d-grid gap-2">
+                    
+                            <button type="button" class="btn btn-outline-secondary btn-block">Home</button>
+                    
+                    </div>
+                </nav>
+            </div>
+
+            <div class="col-9">         
+                <section>
+                    <button type="button" class="btn btn-primary">Ajouter des données</button>
+                    
+                    <?php  include 'includes/form.inc.html'; ?>
+                </section>
+            </div>
+        </div>   
+    </div>
+    
+       
+         
+         <!-- include 'includes/ul.inc.php'; -->
+         
+         
+         
+    <?php include 'includes/footer.inc.html';  ?>
+
+</body>
+</html>
